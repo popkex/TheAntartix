b91d02c2164c8cafbd9c0872a8418f88f942f813
popkex <clementbertrand52@gmail.com>
2024-04-26 21:28:51 +0200

    vérification dossier "saves" existe

--- main.py
+++ main.py
@@ -4,15 +4,6 @@ from inventory import *
 
 pygame.init()
 
-'''
-memo patch note :
-    correction d'un beug d'xp
-    correction beug de l'inventaire
-        les objets s'ajoutais en quantité de 1 à chaque fois
-    modification rapide de la taille de la bar d'exp
-    quand le joueur gagne un combat, il gagne un objet 
-'''
-
 if not os.path.exists(r'saves'):
     os.makedirs(r'saves')
 
