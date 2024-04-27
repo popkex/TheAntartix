203513dba7c2490e84c3fe72536ca7ea10ade082
popkex <clementbertrand52@gmail.com>
2024-04-27 17:04:40 +0200

    supp memo patch (inutile)

--- main.py
+++ main.py
@@ -3,15 +3,6 @@ from game import Game
 
 pygame.init()
 
-'''
-memo patch note :
-    correction d'un beug d'xp
-    correction beug de l'inventaire
-        les objets s'ajoutais en quantité de 1 à chaque fois
-    modification rapide de la taille de la bar d'exp
-    quand le joueur gagne un combat, il gagne un objet 
-'''
-
 game = Game()
 
 # vérifie si le dossier 'saves' existe (si le jeu est pas compiler)
