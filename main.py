4d1dbcc1138b680c1c3a58d84e6bb5e58479ef12
popkex <clementbertrand52@gmail.com>
2024-04-27 17:06:44 +0200

    correction beug création dossier

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ pygame.init()
 game = Game()
 
 # vérifie si le dossier 'saves' existe (si le jeu est pas compiler)
-if not os.path.exists(r'saves') and not os.path.exists(r'_internal'):
-    os.makedirs(r'saves')
-elif not os.path.exists(r'_internal\saves'):
-    os.makedirs(r'_internal\saves')
+if not os.path.exists(r'saves'):
+    if not os.path.exists(r'_internal'):
+        os.makedirs(r'saves')
+    elif not os.path.exists(r'_internal\saves'):
+        os.makedirs(r'_internal\saves')
 
 game.run()
