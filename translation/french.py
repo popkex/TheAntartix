e74de08316009169609acc3709b4e66d154f3eba
popkex <clementbertrand52@gmail.com>
2024-05-02 22:34:50 +0200

    correction de la traduction

--- translation/french.py
+++ translation/french.py
@@ -8,6 +8,6 @@ translations = {
     'object_used': 'Tu as utilisé ',
 
     # les tutoriels
-    'tuto_iv': "Voici ton inventaire.\nIci (comme tout inventaire) tu vas pouvoir stocker des objets et les utilisers.\nAppuie sur 'E' pour pouvoir le fermer et l'ouvrir/fermer quand tu veux.\n\nAppuie sur 'ESC' pour quitter",
+    'tuto_iv': "Voici ton inventaire.\nIci (comme tout inventaire) tu vas pouvoir stocker des objets et les utiliser.\nAppuie sur 'E' pour pouvoir le fermer et l'ouvrir/fermer quand tu veux.\n\nAppuie sur 'ESP' pour quitter",
     'tuto_fight': "Te voilà en en combat !, ici, en appuyant sur 'A' tu peux attaquer ton adversaire\nMais attention, tu te prendras des dégàts. Si tu meurt ta vie reviendras à 50%\net tu perdras de l'experience/diminuera de niveau !\nMais si tu arrive à tuer ton adversaire, tu gagne de l'xp et des objets\nTu peux aussi accéder a ton inventaire en appuyant sur 'E' et fuire\nMais si tu fuis il se peut que tu n'y arrive pas et tu te prendras des dégàts\nBonne chance et va pas te faire tuer\n\nAppuie sur 'ESC' pour quitter"
 }
