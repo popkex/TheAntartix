fd39cecfce4b2346ec6410431c8a913b85c74787
popkex <clementbertrand52@gmail.com>
2024-05-03 17:57:20 +0200

    petit ajustement des stats des enemies

--- entity.py
+++ entity.py
@@ -43,7 +43,7 @@ class EnemyA(Enemy):
         image = pygame.transform.scale(image, (250, 250))
         loot = [(Life_Potion, 3), (Big_Life_Potion, 2)] # (L'objet, le nombre d'objet au max)
 
-        super().__init__("enemy A", loot, image, max_health=50, health=50, attack=12, give_xp=2) # de base 100, 100, 10, 2
+        super().__init__("enemy A", loot, image, max_health=50, health=50, attack=12, give_xp=4) # de base 100, 100, 10, 2
 
 class EnemyB(Enemy):
     def __init__(self, game):
@@ -52,4 +52,4 @@ class EnemyB(Enemy):
         image = pygame.transform.scale(image, (250, 250))
         loot = [(Big_Life_Potion, 3), (Bomb, 1)] # (L'objet, le nombre d'objet au max)
 
-        super().__init__("enemy B", loot, image, max_health=80, health=80, attack=15, give_xp=5) # de base 120, 120, 12, 5
+        super().__init__("enemy B", loot, image, max_health=60, health=60, attack=12, give_xp=6) # de base 120, 120, 12, 5
