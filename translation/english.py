2e3350cc5a86be730d7ccfabe42eb4cf65e58190
popkex <clementbertrand52@gmail.com>
2024-05-03 23:41:34 +0200

    ajout de settings et possibilité de changer de langue

--- translation/english.py
+++ translation/english.py
@@ -9,5 +9,8 @@ translations = {
 
     # the tutorials
     'tuto_iv': "This is your inventory.\nLike any other inventory, you can store items and use them here.\nPress 'E' to open and close it whenever you want.\n\nPress 'ESC' to exit",
-    'tuto_fight': "You're in a fight! Here, by pressing 'A' you can attack your opponent.\nBut be careful, you will take damage. If you die, your health will return to 50%\nand you will lose experience/level down!\nBut if you manage to kill your opponent, you gain xp and items\nYou can also access your inventory by pressing 'E' and flee\nBut if you flee, you might not make it and you will take damage\nGood luck and don't get killed\n\nPress 'ESC' to exit"
+    'tuto_fight': "You're in a fight! Here, by pressing 'A' you can attack your opponent.\nBut be careful, you will take damage. If you die, your health will return to 50%\nand you will lose experience/level down!\nBut if you manage to kill your opponent, you gain xp and items\nYou can also access your inventory by pressing 'E' and flee\nBut if you flee, you might not make it and you will take damage\nGood luck and don't get killed\n\nPress 'ESC' to exit",
+
+    # le menu pause
+    'main_pause_menu_title': 'Pause Menu',
 }
--- translation/french.py
+++ translation/french.py
@@ -9,5 +9,16 @@ translations = {
 
     # les tutoriels
     'tuto_iv': "Voici ton inventaire.\nIci (comme tout inventaire) tu vas pouvoir stocker des objets et les utiliser.\nAppuie sur 'E' pour pouvoir le fermer et l'ouvrir/fermer quand tu veux.\n\nAppuie sur 'ESP' pour quitter",
-    'tuto_fight': "Te voilà en en combat !, ici, en appuyant sur 'A' tu peux attaquer ton adversaire\nMais attention, tu te prendras des dégàts. Si tu meurt ta vie reviendras à 50%\net tu perdras de l'experience/diminuera de niveau !\nMais si tu arrive à tuer ton adversaire, tu gagne de l'xp et des objets\nTu peux aussi accéder a ton inventaire en appuyant sur 'E' et fuire\nMais si tu fuis il se peut que tu n'y arrive pas et tu te prendras des dégàts\nBonne chance et va pas te faire tuer\n\nAppuie sur 'ESC' pour quitter"
+    'tuto_fight': "Te voilà en en combat !, ici, en appuyant sur 'A' tu peux attaquer ton adversaire\nMais attention, tu te prendras des dégàts. Si tu meurt ta vie reviendras à 50%\net tu perdras de l'experience/diminuera de niveau !\nMais si tu arrive à tuer ton adversaire, tu gagne de l'xp et des objets\nTu peux aussi accéder a ton inventaire en appuyant sur 'E' et fuire\nMais si tu fuis il se peut que tu n'y arrive pas et tu te prendras des dégàts\nBonne chance et va pas te faire tuer\n\nAppuie sur 'ESC' pour quitter",
+
+    # le menu pause
+    'main_pause_menu_title': 'Menu Pause',
+    'settings_button': 'Paramètres',
+    'main_settings_title': 'Paramètres',
+    'language': 'Langues',
+    'main_settings_languages_title': 'Paramètres de langue',
+
+    # Les langues
+    'french': 'Francais',
+    'english': 'Anglais',
 }
