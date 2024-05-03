ce5822ecb20d999d6205056270a4d624da4e7c61
popkex <clementbertrand52@gmail.com>
2024-05-04 00:23:43 +0200

    modification mise en forme des traductions

--- translation/english.py
+++ translation/english.py
@@ -1,16 +1,39 @@
 translations = {
-    # objets
-    'Life_Potion': 'a health potion',
-    'Big_Life_Potion': 'a big health potion',
-    'Bomb': 'a bomb',
 
-    # utilisations
-    'object_used': 'You used ',
+    'message_system': {
+        # utilisations
+        'object_used': 'You used ',
+    },
 
-    # the tutorials
-    'tuto_iv': "This is your inventory.\nLike any other inventory, you can store items and use them here.\nPress 'E' to open and close it whenever you want.\n\nPress 'ESC' to exit",
-    'tuto_fight': "You're in a fight! Here, by pressing 'A' you can attack your opponent.\nBut be careful, you will take damage. If you die, your health will return to 50%\nand you will lose experience/level down!\nBut if you manage to kill your opponent, you gain xp and items\nYou can also access your inventory by pressing 'E' and flee\nBut if you flee, you might not make it and you will take damage\nGood luck and don't get killed\n\nPress 'ESC' to exit",
+    'objects': {
+        'Life_Potion': 'a health potion',
+        'Big_Life_Potion': 'a big health potion',
+        'Bomb': 'a bomb',
+    },
+
+    # les tutoriels
+    'tutorials': {
+        'tuto_iv': "Here is your inventory.\nHere (like any inventory), you will be able to store items and use them.\nPress 'E' to open/close it whenever you want.\n\nPress 'ESP' to exit.",
+        'tuto_fight': "You are now in battle! Here, by pressing 'A', you can attack your opponent.\nBut be careful, you will take damage. If you die, your health will return to 50%\nand you will lose experience/level down!\nBut if you manage to kill your opponent, you will gain experience and items.\nYou can also access your inventory by pressing 'E' and flee.\nBut if you flee, you may not succeed and you will take damage.\nGood luck and don't get killed!\n\nPress 'ESC' to quit.",
+    },
 
     # le menu pause
-    'main_pause_menu_title': 'Pause Menu',
+    'pause_menu': {
+        'title': 'Pause Menu',
+        'settings_button': 'Settings',
+    },
+
+    'settings_menu': {
+        'title': 'Settings',
+        'language': 'Language',
+    },
+
+    'settings_languages_menu': {
+        'title': 'Language Settings',
+    },
+
+    'languages': {
+        'french': 'French',
+        'english': 'English',
+    }
 }
--- translation/french.py
+++ translation/french.py
@@ -1,24 +1,37 @@
 translations = {
-    # objets
-    'Life_Potion': 'une potion de soins',
-    'Big_Life_Potion': 'une grosse potion de soins',
-    'Bomb': 'une bombe',
 
-    # utilisations
-    'object_used': 'Tu as utilisé ',
+    'message_system': {
+        'object_used': 'Tu as utilisé',
+    },
 
-    # les tutoriels
-    'tuto_iv': "Voici ton inventaire.\nIci (comme tout inventaire) tu vas pouvoir stocker des objets et les utiliser.\nAppuie sur 'E' pour pouvoir le fermer et l'ouvrir/fermer quand tu veux.\n\nAppuie sur 'ESP' pour quitter",
-    'tuto_fight': "Te voilà en en combat !, ici, en appuyant sur 'A' tu peux attaquer ton adversaire\nMais attention, tu te prendras des dégàts. Si tu meurt ta vie reviendras à 50%\net tu perdras de l'experience/diminuera de niveau !\nMais si tu arrive à tuer ton adversaire, tu gagne de l'xp et des objets\nTu peux aussi accéder a ton inventaire en appuyant sur 'E' et fuire\nMais si tu fuis il se peut que tu n'y arrive pas et tu te prendras des dégàts\nBonne chance et va pas te faire tuer\n\nAppuie sur 'ESC' pour quitter",
+    'objects': {
+        'Life_Potion': 'une potion de soin',
+        'Big_Life_Potion': 'une grosse potion de soin',
+        'Bomb': 'une bombe',
+    },
+
+    'tutorials': {
+        'tuto_iv': "Voici ton inventaire.\nIci (comme dans tout inventaire), tu vas pouvoir stocker des objets et les utiliser.\nAppuie sur 'E' pour pouvoir le fermer et l'ouvrir/fermer quand tu veux.\n\nAppuie sur 'ESP' pour quitter.",
+        'tuto_fight': "Te voilà en combat ! Ici, en appuyant sur 'A', tu peux attaquer ton adversaire.\nMais attention, tu te prendras des dégâts. Si tu meurs, ta vie reviendra à 50% et tu perdras de l'expérience/diminueras de niveau !\nMais si tu arrives à tuer ton adversaire, tu gagnes de l'expérience et des objets.\nTu peux aussi accéder à ton inventaire en appuyant sur 'E' et fuir.\nMais si tu fuis, il se peut que tu n'y arrives pas et tu te prendras des dégâts.\nBonne chance et ne te fais pas tuer !\n\nAppuie sur 'ESC' pour quitter.",
+    },
 
     # le menu pause
-    'main_pause_menu_title': 'Menu Pause',
-    'settings_button': 'Paramètres',
-    'main_settings_title': 'Paramètres',
-    'language': 'Langues',
-    'main_settings_languages_title': 'Paramètres de langue',
+    'pause_menu': {
+        'title': 'Menu Pause',
+        'settings_button': 'Paramètres',
+    },
 
-    # Les langues
-    'french': 'Francais',
-    'english': 'Anglais',
+    'settings_menu': {
+        'title': 'Paramètres',
+        'language': 'Langue',
+    },
+
+    'settings_languages_menu': {
+        'title': 'Paramètres de langue',
+    },
+
+    'languages': {
+        'french': 'Français',
+        'english': 'Anglais',
+    }
 }
