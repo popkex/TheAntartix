1cc44c607bde6809d8cb4dd0d16daa9a55ddca00
popkex <clementbertrand52@gmail.com>
2024-05-18 16:53:12 +0200

    actualisation de la langue anglaise

--- translation/english.py
+++ translation/english.py
@@ -10,30 +10,30 @@ translations = {
     },
 
     "enemyA":{
-        "name": "enemy A",
-        "lanch_fight_message": "Enemy A has fallen on you",
+        'name': "enemy A",
+        'lanch_fight_message': "Enemy A has fallen on you",
     },
 
     "enemyB": {
         "name": "enemy B",
-        'lanch_fight_message': "An enemy B has appeared all of a sudden",
+        'lanch_fight_message': "An enemy B appeared all of a sudden",
     },
 
     "objects": {
-        "Life_Potion": "a health potion",
-        "Big_Life_Potion": "a big health potion",
+        "Life_Potion": "a healing potion",
+        "Big_Life_Potion": "a big healing potion",
         "Bomb": "a bomb",
     },
 
     'npc': {
-        # a list containing all the dialogues of the npc
-        'paul': ['Hello, I am Paul', 'How are you?', 'Who are you?', "I'm afraid of monsters", "I would love if someone could help me get rid \nof these monsters", "About twenty?\nThank you, I will reward you well"],
-        'michel': ['I am seriously injured because of my\nexcursion in this dungeon.', "It's a pity that I didn't have more healing potions \notherwise I wouldn't be in this state", "You'd better get some, believe me"],
+        # a list containing all the npc dialogues
+        'paul': ['Hello, I am Paul', 'How are you?', 'Who are you?', "I'm afraid of monsters", "I would love for someone to help me get rid \nof these monsters", "About twenty?\nThank you, I will reward you well"],
+        'michel': ['I am seriously injured because of my\nexcursion in this dungeon.', "Too bad I didn't have any more healing potion \notherwise I wouldn't be in this state", "You'd better get some, believe me"],
     },
 
     'michel_quest': {
-        'title': 'Get healing potions to not get hurt',
-        'description': 'Get 10 healing potions to not get hurt in combat like this poor Michel'
+        'title': 'Get healing potions so you do not get hurt',
+        'description': 'Get 10 healing potions so you do not get hurt in combat like poor Michel'
     },
 
     'paul_quest': {
@@ -41,9 +41,18 @@ translations = {
         'description': "Kill 20 monsters of any type",
     },
 
+    'michel': {
+        "name": 'michel',
+    },
+
+    'paul': {
+        "name": 'paul',
+    },
+
     "tutorials": {
-        "tuto_iv": ["Here is your inventory.", "Here (like any inventory),\nyou will be able to store items and use them.", "You can press 'E' to be able to \nclose it and open/close it whenever you want."],
-        "tuto_fight": ["You are now in combat!", "Here, by pressing 'A', you can attack your \nopponent. But be careful, you will take damage.", "If you die, your life will return to 50% of your life\nand you will lose experience/level down!", "But if you manage to kill your opponent, \nyou gain experience and items.", "You can also access your inventory by pressing \n'E' and flee with 'ESC'.", "I'm going to tell you a secret, you can also press \nthe icons with your mouse to execute the action", "But be careful, if you flee, you may not \nsucceed and you will take damage.", "Good luck and don't get killed!"],
+        "tutorial": "Tutorial",
+        "tuto_iv": ["Here is your inventory.", "Here (like in any inventory),\nyou will be able to store objects and use them.", "You can press 'E' to be able to \nclose it and open/close it whenever you want."],
+        "tuto_fight": ["You are in combat!", "Here, by pressing 'A', you can attack your \nopponent. But beware, you will take \ndamage.", "If you die, your life will return to 50% of your life\nand you will lose experience/level down!", "But if you manage to kill your opponent, \nyou gain experience and objects.", "You can also access your inventory by pressing \n'E' and escape with 'ESC'.", "I'm going to tell you a secret, you can also press \non the icons with your mouse to execute the action", "But beware, if you flee, you may not \nmake it and you will take damage.", "Good luck and don't get killed!"],
     },
 
     "main_menu": {
@@ -58,21 +67,31 @@ translations = {
         "title": "Pause Menu",
         "quest_button": 'Quests',
         "settings_button": "Settings",
+        "tutorial_button": "Tutorials",
+        "back_to_the_game": "Back",
         'save_and_main_menu': 'Save and return to the main menu',
         'save_and_quit_button': "Save and quit",
     },
 
+    'tutorial_menu': {
+        "title": "Tutorials",
+        "back": "Back",
+    },
+
     "quest_menu": {
         'title': "Quests",
+        "back": "Back",
     },
 
     "settings_menu": {
         "title": "Settings",
         "language": "Language",
+        "back": "Back",
     },
 
     "settings_languages_menu": {
         "title": "Language Settings",
+        "back": "Back",
     },
 
     "languages": {
@@ -86,12 +105,12 @@ translations = {
         'fight_message_2': "Another brave soul... crushed by the reality of 'Game Over'.",
         'fight_message_3': "He tried, he failed, and now he's part of the scenery.",
         'fight_message_4': "He ran towards glory, but he tripped over his sword.",
-        'fight_message_5': "He jumped into the battle... and directly into a trap.",
+        'fight_message_5': "He jumped into battle... and straight into a trap.",
         'fight_message_6': "He shouted 'For honor!', but all we heard was 'Ouch!'",
         'fight_message_7': "He fought like a lion, but alas, he was just a kitten.",
         'fight_message_8': "He sought victory, but found a picturesque end.",
         'fight_message_9': "He dreamed of being invincible, he forgot to practice it.",
-        'fight_message_10': "He wanted to be the game master, but the game mastered him.",
+        'fight_message_10': "He wanted to be the master of the game, but the game mastered him.",
     },
 
     'button_press': {
