6d9089f9237e021479a4f3088dee6a3bb59b4bca
popkex <clementbertrand52@gmail.com>
2024-07-09 21:20:58 +0200

    mise a jour des traductions

--- translation/english.py
+++ translation/english.py
@@ -1,90 +1,96 @@
 translations = {
+
     "message_system": {
-        "object_used": "You have used",
-        "damage_suffered1": "You have suffered",
+        "object_used": "You used",
+        "damage_suffered1": "You suffered",
         "damage_suffered2": "damage",
-        "damage_crit_suffered1": "You have suffered",
+        "damage_crit_suffered1": "You suffered",
         "damage_crit_suffered2": "critical damage!",
         "failed_escape": "You failed to escape",
+        'defense': 'You managed to raise your shield in time!',
+        'fail_defense': "Unfortunately, you failed to block the attack",
         'player_attack': "You attacked!",
         'player_crit': "You dealt a critical hit!",
+        'player_fail_attack': 'You missed your attack',
+        'enemy_fail_attack': "Your enemy failed to hit you!",
         "enemy_known_out_player": "You knocked out your enemy!",
         'recovered_object': "You recovered",
+        'player_dont_have_enough_energy_for_attack': "You don't have enough energy to attack",
         'auto_save': "Auto save",
         'exit': "to exit",
     },
 
     "enemyA": {
-        'name': "Enemy A",
-        'lanch_fight_message': "Enemy A stumbled upon you",
+        'name': "enemy A",
+        'lanch_fight_message': "Enemy A encountered you",
     },
 
     "enemyB": {
-        "name": "Enemy B",
-        'lanch_fight_message': "An Enemy B appeared by surprise",
+        "name": "enemy B",
+        'lanch_fight_message': "Enemy B appeared by surprise",
     },
 
     "objects": {
         "Life_Potion": "healing potion",
-        "Big_Life_Potion": "large healing potion",
+        "Big_Life_Potion": "big healing potion",
         "Bomb": "bombs",
     },
 
     'npc': {
         'launch_dialog': "Press Space",
-        'paul': ["Hello, I am Paul", "How are you?", "Who are you?", "I am afraid of monsters", "I would love someone to help me get rid of these monsters.", "About twenty? Thanks, I will reward you well"],
+        'paul': ["Hello, I'm Paul", "How are you?", "Who are you?", "I'm afraid of monsters", "I'd love for someone to help me get rid of \nthese monsters.", "About twenty? \nThank you, I'll reward you well"],
         "paul_after_quest": ["test, test"],
-        'michel': ["I am seriously injured because of my excursion into this dungeon.", "Too bad I didn't have any healing potion otherwise I wouldn't be in this state", "You better get some, believe me"],
+        'michel': ["I'm badly injured from my \nexcursion in this dungeon.", "Too bad I didn't have a healing potion \notherwise I wouldn't be in this state", "You better get some, trust me"],
         "michel_after_quest": ["test"],
-        'fleufleu': ["Days come and go, but it’s in the little things that I find my joy.", "A shared smile, the warmth of the sun on my skin, the melody of a distant song...",  "These fleeting moments are treasures"]
+        'fleufleu': ["The days go by and look alike, \nbut it's in the little things \nthat I find my happiness.", "A shared smile, \nthe warmth of the sun on my skin, \nthe melody of a distant song...", "These fleeting moments are treasures"]
     },
 
     'michel_quest': {
-        'title': "Collect healing potions to avoid getting hurt",
-        'description': "Collect 10 healing potions to avoid getting hurt in battle like poor Michel"
+        'title': "Get healing potions to avoid getting hurt",
+        'description': "Get 10 healing potions to avoid getting hurt in combat like poor Michel"
     },
 
     'paul_quest': {
-        'title': "Kill monsters so Paul is no longer afraid",
+        'title': "Kill monsters so Paul won't be scared",
         'description': "Kill 20 monsters of any type",
     },
 
     'michel': {
-        "name": "Michel",
+        "name": "michel",
     },
 
     'paul': {
-        "name": "Paul",
+        "name": "paul",
     },
 
     'fleufleu': {
-        "name": "Fleufleu",
+        "name": "fleufleu",
     },
 
     "victory": {
         "title": "Victory!",
-        "victory0": "Congratulations! You triumphed brilliantly!",
-        "victory1": "Stunning victory! You dominated the game with style.",
+        "victory0": "Congratulations! You have brilliantly triumphed!",
+        "victory1": "Outstanding victory! You dominated the game with style.",
         "victory2": "Mission accomplished! You are the hero of the day.",
         "victory3": "Total success! Your strategy paid off.",
         "victory4": "Glory to you! You crushed your opponents.",
-        "victory5": "Legendary triumph! You enter the annals of history.",
+        "victory5": "Legendary triumph! You enter the history books.",
         "victory6": "Champions! You overcame all obstacles.",
-        "victory7": "Bravo! Your mastery is undeniable.",
+        "victory7": "Bravissimo! Your mastery is undeniable.",
         "victory8": "Assured victory! You showed courage and tenacity.",
-        "victory9": "Remarkable feat! You won with brilliance.",
+        "victory9": "Remarkable feat! You won with flair.",
 
-        "enemy_death": "You have defeated:",
-        "object_won": "You have won",
-        "not_object_won": "You didn't manage to recover an object",
-        "xp_won": "You have gained",
+        "enemy_death": "You defeated:",
+        "object_won": "You won",
+        "not_object_won": "You didn't manage to get an item",
+        "xp_won": "You gained",
         "exit": "Press any key to exit",
     },
 
     "tutorials": {
         "tutorial": "Tutorial",
-        "tuto_iv": ["Here is your inventory.", "Here (like in any inventory), you will be able to store items and use them.", "You can press 'E' to open and close it at any time."],
-        "tuto_fight": ["Here you are in battle!", "Here, by pressing 'A', you can attack your opponent. But beware, you will take damage.", "If you die, your life will return to 50% and you will lose experience/level!", "But if you manage to kill your opponent, you gain experience and items.", "You can also access your inventory by pressing 'E' and flee with 'ESC'.", "I'll tell you a secret, you can also click on the icons with your mouse to execute the action", "But beware, if you flee, you might not succeed and you will take damage.", "Good luck and don't get killed!"],
+        "tuto_iv": ["Here is your inventory.", "Here (as in any inventory), \nyou will be able to store items and use them.", "You can press 'E' to open and close it anytime."],
+        "tuto_fight": ["Here you are in combat!", "Here, by pressing 'A', you can attack your \nopponent. In return, you will take damage.", "If you die, your life will return to 50% of your max life, \nyou will lose experience/level down!", "And you will restart at the beginning of the \nlevel! Although all interactions with \nthe map will remain", "But if you manage to kill your opponent, \nyou will gain experience and items.", "You can also defend yourself with 'Z', reducing the \ndamage taken by a % that can vary over time", "open your inventory by pressing 'E' \nand escape with 'ESC'.", "I'll tell you a secret, you can also click \non the icons with your mouse to perform the action", "But beware, if you flee, you might not \nsucceed and you will take damage.", "Good luck and don't get killed!"],
     },
 
     "main_menu": {
@@ -103,7 +109,7 @@ translations = {
 
     'confirm_reset_game': {
         'title': "Are you sure you want to start a new game?",
-        'title2': "You will not be able to undo this",
+        'title2': "You can't undo this",
         'confirm': "Confirm",
         'cancel': "Cancel",
     },
@@ -113,8 +119,8 @@ translations = {
         "quest_button": "Quests",
         "settings_button": "Settings",
         "tutorial_button": "Tutorials",
-        "back_to_the_game": "Back to Game",
-        'save_and_quit_button': "Save and Quit",
+        "back_to_the_game": "Back",
+        'save_and_quit_button': "Save and quit",
     },
 
     'tutorial_menu': {
@@ -154,7 +160,7 @@ translations = {
         "15min": "15 minutes",
         "30min": "30 minutes",
         "1h": "1 hour",
-        "desactivated": "Disabled",
+        "desactivated": "Deactivate",
         "actualy_time": "currently set to:",
         "back": "Back",
     },
@@ -166,16 +172,16 @@ translations = {
     },
 
     'game_over': {
-        'fight_message_1': "He wanted to be the hero, but ended up a zero.",
+        'fight_message_1': "He wanted to be a hero, but ended up a zero.",
         'fight_message_2': "Another brave soul... crushed by the reality of 'Game Over'.",
         'fight_message_3': "He tried, he failed, and now he's part of the scenery.",
-        'fight_message_4': "He ran towards glory, but tripped over his sword.",
-        'fight_message_5': "He jumped into battle... and straight into a trap.",
+        'fight_message_4': "He ran towards glory, but stumbled over his own sword.",
+        'fight_message_5': "He jumped into battle... and directly into a trap.",
         'fight_message_6': "He yelled 'For honor!', but all we heard was 'Ouch!'",
         'fight_message_7': "He fought like a lion, but alas, he was just a kitten.",
         'fight_message_8': "He sought victory, but found a picturesque end.",
-        'fight_message_9': "aaw, why don't you buy a better aimbot",
-        'fight_message_10': "He wanted to master the game, but the game mastered him.",
+        'fight_message_9': "hmm you should have bought a better aimbot.",
+        'fight_message_10': "He wanted to be the master of the game, but the game mastered him.",
     },
 
     'button_press': {
