fc1be1dd0882b810eb0ca209a2c261a78a6558e9
popkex <clementbertrand52@gmail.com>
2024-07-09 18:44:01 +0200

    modification du tutoriel fight

--- translation/french.py
+++ translation/french.py
@@ -90,7 +90,7 @@ translations = {
     "tutorials": {
         "tutorial": "Tutoriel",
         "tuto_iv": ["Voici ton inventaire.", "Ici (comme dans tout inventaire),\ntu vas pouvoir stocker des objets et les utiliser.", "Tu peux appuyer sur 'E' pour le fermer et l'ouvrir/fermer quand tu veux."],
-        "tuto_fight": ["Te voilà en combat !", "Ici, en appuyant sur 'A', tu peux attaquer ton \nadversaire. Mais attention, tu te prendras des \ndégâts.", "Si tu meurs, ta vie reviendra à 50% de ta vie max\net tu perdras de l'expérience/diminueras de niveau !", "Mais si tu arrives à tuer ton adversaire, \ntu gagnes de l'expérience et des objets.", "Tu peux aussi accéder à ton inventaire en appuyant \nsur 'E' et fuir avec 'ECHAP'.", "Je vais te dire un secret, tu peux aussi appuyer \nsur les icônes avec ta souris pour exécuter l'action", "Mais attention, si tu fuis, il se peut que tu n'y \narrives pas et tu te prendras des dégâts.", "Bonne chance et ne te fais pas tuer !"],
+        "tuto_fight": ["Te voilà en combat !", "Ici, en appuyant sur 'A', tu peux attaquer ton \nadversaire. En retour tu auras des dégàts.", "Si tu meurs, ta vie reviendra à 50% de ta vie max\net tu perdras de l'expérience/diminueras de niveau !", "Mais si tu arrives à tuer ton adversaire, \ntu gagnes de l'expérience et des objets.", "Tu peux aussi te défendre avec  'Z', tu réduira les\ndégàts subit d'un % qui pourra varié avec le temps", "ouvre ton inventaire en appuyant sur 'E\net fuit avec 'ECHAP'.", "Je vais te dire un secret, tu peux aussi appuyer \nsur les icônes avec ta souris pour exécuter l'action", "Mais attention, si tu fuis, il se peut que tu n'y \narrives pas et tu te prendras des dégâts.", "Bonne chance et ne te fais pas tuer !"],
     },
 
     "main_menu": {
